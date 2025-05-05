390e5cf82443ddf546cdbe7fb4ecedf94b350e5d
SantosFMatheus <matheusantosfp@gmail.com>
2025-05-05 20:15:55 -0300

    first

--- src/main/kotlin/com/imepac/imepet/OrcamentoApplication.kt
+++ src/main/kotlin/com/imepac/imepet/OrcamentoApplication.kt
@@ -1,4 +1,4 @@
-package com.finan.orcamento
+package com.imepac.imepet
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
---
+++ src/main/kotlin/com/imepac/imepet/model/UsuarioModel.java
@@ -0,0 +1,36 @@
+package com.imepac.imepet.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class UsuarioModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+    private String telefone;
+
+    public UsuarioModel() {}
+
+    public UsuarioModel(String nome, String email, String telefone) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    // Getters e Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getTelefone() { return telefone; }
+    public void setTelefone(String telefone) { this.telefone = telefone; }
+}
